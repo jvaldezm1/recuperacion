82e571ef9b88a75b92532148ab1d169e25426cd4
jvaldezm1 <jvaldezm1@miumg.edu.gt>
2023-06-24 10:23:15 -0600

    se crea una interfaz con 2 cajas de texto

--- JavaApplication19/src/javaapplication19/claseHijaFinal.java
+++ JavaApplication19/src/javaapplication19/claseHijaFinal.java
@@ -4,7 +4,14 @@ package javaapplication19;
 
 public class claseHijaFinal extends EjerciciosFinal {
     
-    
-    
+  public   String nombreClaseHija;
+   public String cantidadEjercicios;
+  
+   public String muestra (String nombreClaseHija,String cantidadEjercicios){
+   String resultado;
+   resultado = nombreClaseHija + cantidadEjercicios ;
+   return resultado;
+ 
+   }
     
 }
